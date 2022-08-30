492667fa2e05ccab01f43d275626f59513646ec3
barry cumbie <barrycumbie@gmail.com>
2022-08-30 11:46:17 -0500

    data back forth with ejs

--- hello-node.js
+++ hello-node.js
@@ -1,15 +1,29 @@
 const express = require('express')
-const app = express()
-const path = require('node:path')
+const path = require('path')
+const bodyParser = require('body-parser')
+
+const app = express();
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set('view engine', 'ejs');
 
 console.log('hello from node');
 
-app.get('/', function (req, res) {
-  // res.send('<h2>Hello World fr1om Express</h2>')
+var thatData = '   ';
 
-  res.sendFile(path.join(__dirname, 'index.html'));
+app.get('/', function(req, res) {
+    // res.send('<h2>Hello World fr1om Express</h2>')
+    // res.sendFile(path.join(__dirname, 'index.html'));
+    res.render('index', { thatData: req.body.thatData });
 
 })
 
-app.listen(process.env.PORT || 3000, 
-	() => console.log("Server is running..."));
+app.post("/saveToNode", (req, res) => {
+    console.log(req.body);
+    console.group(req.body.thatData);
+    res.render('index', { thatData: req.body.thatData });
+
+})
+
+app.listen(process.env.PORT || 3000,
+    () => console.log("Server is running..."));
